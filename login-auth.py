698545773fd8d6f260edbdac99a007d2f93fdb21
Yjn024 <jiening.yu@outlook.com>
2022-09-26 22:16:43 +0800

    data system

--- login-auth.py
+++ login-auth.py
@@ -1,7 +1,26 @@
+import hashlib
+import os
 import random
 
-user = input("Set your username: ")
-password = input("Set your password: ")
+def md5(input):
+    return hashlib.md5(input.encode()).hexdigest().strip()
+def hash_int(input):
+    return int(hash(input))
+
+pathT = os.getcwd() + "/data"
+
+if (not os.path.exists(pathT)):
+    os.makedirs(pathT)
+    user = input("Set your username: ")
+    password = input("Set your password: ")
+    uf = open(pathT + "/username.txt", "w")
+
+    uf.write(md5(user))
+    uf.close()
+    pf = open(pathT + "/password.txt", "w")
+
+    pf.write(md5(password))
+    pf.close()
 
 success = False
 count = 0
@@ -10,7 +29,17 @@ while not success:
     user_auth = input("Input your username: ")
     password_auth = input("Input your password: ")
     count += 1
-    if user_auth == user and password_auth == password:
+
+    uf = open(pathT + "/username.txt", "r")
+    pf = open(pathT + "/password.txt", "r")
+
+    aa = hash_int(md5(user_auth))
+    ab = hash_int(uf.read().strip())
+
+    ba = hash_int(md5(password_auth))
+    bb = hash_int(pf.read().strip())
+
+    if aa == ab and ba == bb:
         success = True
         break
     else:
