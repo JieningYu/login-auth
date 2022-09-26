699e1ac9645626a81f019b1bb3f48b5e16b77ae9
Yjn024 <jiening.yu@outlook.com>
2022-09-26 16:42:46 +0800

    simple

---
+++ login-auth.py
@@ -0,0 +1,27 @@
+import os
+import random
+
+folder = os.path.exists
+
+user = input("Set your username: ")
+password = input("Set your password: ")
+
+success = False
+count = 0
+
+while not success:
+    user_auth = input("Input your username: ")
+    password_auth = input("Input your password: ")
+    count += 1
+    if user_auth == user and password_auth == password:
+        success = True
+        break
+    else:
+        print("Username of password is wrong!")
+    
+    if count > random.randint(3, 5):
+        print("Tried too many times!")
+        break
+
+if success:
+    print("Login successfully!")
